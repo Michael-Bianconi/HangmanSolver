2e6693bdb24a2bc24146f31f97413f119aed1f5b
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-06-08 20:16:09 -0600

    Removed extraneous code

--- arraylist.h
+++ arraylist.h
@@ -42,63 +42,6 @@ void ArrayList_add(ArrayList list, char* item);
 
 
 /**
- * Adds all given arguments to the list.
- *
- * @param list List to add to.
- * @param argc Number of given arguments.
- * @param ... Items to add (must be char*).
- */
-void ArrayList_addAll(ArrayList list, int argc, ...);
-
-
-/**
- * Adds all elements in the array to the list.
- *
- * @param dest List to add to.
- * @param n Number of items in the array.
- * @param items Array to add from.
- */
-void ArrayList_addArray(ArrayList dest, size_t n, char** items);
-
-
-/**
- * Adds all elements from source list into destination list.
- *
- * @param dest List to add to.
- * @param src List to add from.
- */
-void ArrayList_addList(ArrayList dest, ArrayList src);
-
-
-/**
- * Removes all items from the list.
- *
- * @param list List to alter.
- */
-void ArrayList_clear(ArrayList list);
-
-
-/**
- * Checks if the given string exists in the list.
- *
- * @param list List to check.
- * @param item Item to search for.
- * @return Returns 1 if present, 0 otherwise.
- */
-unsigned short ArrayList_contains(ArrayList list, char* item);
-
-
-/**
- * Construct a new ArrayList with the same data as
- * the given ArrayList.
- *
- * @param list List to copy.
- * @return Returns a new ArrayList.
- */
-ArrayList ArrayList_copy(ArrayList list);
-
-
-/**
  * Construct a new ArrayList with the default given size.
  * 
  * @param size Allocate enough space to hold this many items.
@@ -107,28 +50,6 @@ ArrayList ArrayList_copy(ArrayList list);
  * @return Returns the constructed ArrayList.
  */
 ArrayList ArrayList_create(size_t size);
-
-
-/**
- * Takes the first N items in the array and adds them to
- * a newly created list.
- *
- * @param n Number of items to add (must be <= array size).
- * @param array Array to add from.
- * @return Returns newly created list.
- */
-ArrayList ArrayList_createFromArray(size_t n, char** array);
-
-
-/**
- * Checks if each item is the same across lists. Does not
- * check buffer size.
- *
- * @param a First list.
- * @param b Second list.
- * @return returns 1 if equal, 0 otherwise.
- */
-unsigned short ArrayList_equals(ArrayList a, ArrayList b);
 
 
 /**
@@ -161,42 +82,6 @@ char* ArrayList_get(ArrayList list, size_t n);
 
 
 /**
- * Inserts the item into the array and shifts
- * all further items up by 1.
- *
- * @param list List to alter.
- * @param item Item to add.
- * @param size_t idx Position to insert.
- */
-void ArrayList_insert(ArrayList list, char* item, size_t idx);
-
-
-/**
- * Print the contents of the ArrayList.
- *
- * @param list ArrayList to print.
- */
-void ArrayList_print(ArrayList list);
-
-
-/**
- * Creates a new ArrayList with all the items in list that
- * are within the given range. If the original list is
- * verbose, the sublist will be too.
- *
- * If start is greater than end, no items will be copied.
- * If end is greater than the size of the list, all item
- * after start will be copied.
- *
- * @param list List to select from.
- * @param start The beginning of the range, inclusive.
- * @param end The end of the range, exclusive.
- * @return Returns a new ArrayList.
- */
-ArrayList ArrayList_range(ArrayList list, size_t start, size_t end);
-
-
-/**
  * Removes an element from the ArrayList. Shifts all others down
  * to fill the gap. Does not resize the array.
  *
@@ -204,60 +89,6 @@ ArrayList ArrayList_range(ArrayList list, size_t start, size_t end);
  * @param n Remove the n'th item in the list.
  */
 void ArrayList_remove(ArrayList list, size_t n);
-
-
-/**
- * Remove all items from the list that match the given item.
- *
- * @param list List to alter.
- * @param match Item to match.
- */
-void ArrayList_removeAllMatches(ArrayList list, char* match);
-
-
-/**
- * Reverse all items in the list.
- *
- * @param list List to reverse.
- */
-void ArrayList_reverse(ArrayList list);
-
-
-/**
- * Set the given item to the given value.
- *
- * @param list List to alter.
- * @param item Item to set.
- * @param n N'th index.
- */
-void ArrayList_set(ArrayList list, char* item, size_t n);
-
-
-/**
- * Shuffles the ArrayList using a Fisher-Yates shuffle.
- *
- * @param list List to shuffle.
- */
-void ArrayList_shuffle(ArrayList list);
-
-
-/**
- * Sorts the list lexigraphically, in-place,
- * using quick sort, in ascending order.
- *
- * @param list List to sort.
- */
-void ArrayList_sort(ArrayList list);
-
-
-/**
- * Swaps the two given elements with each other.
- *
- * @param list List to alter.
- * @param i First index.
- * @param j Second index.
- */
-void ArrayList_swap(ArrayList list, size_t i, size_t j);
 
 
 /**
